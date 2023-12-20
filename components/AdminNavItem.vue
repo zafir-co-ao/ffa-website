cb43da3201cd2c083f80e413850f457639fb80d0
Dzerjinski Lemos <dzerjinski.lemos@zafir.co.ao>
2023-12-20 17:45:35 +0100

    Update dependencies and URLs

--- components/AdminNavItem.vue
+++ components/AdminNavItem.vue
@@ -6,9 +6,6 @@ defineProps<{ title: string; url: string }>();
 <template>
 	<li class="nav-item">
 		<NuxtLink :to="url" class="nav-link" aria-current="page">
-			<svg class="bi me-2" width="16" height="16">
-				<use xlink:href="#home" />
-			</svg>
 			{{ title }}
 		</NuxtLink>
 	</li>
@@ -16,7 +13,7 @@ defineProps<{ title: string; url: string }>();
 
 <style scoped>
 li {
-	padding-left: 0;
+	padding: 0.5rem 0;
 }
 li::before {
 	content: none !important;
@@ -27,11 +24,5 @@ li::before {
 
 .nav-link:hover {
 	background-color: #0d6efd;
-}
-
-.bi {
-	vertical-align: -0.125em;
-	pointer-events: none;
-	fill: currentColor;
 }
 </style>
