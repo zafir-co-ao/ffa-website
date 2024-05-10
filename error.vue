cfc42087aca75fe3de1d3a1b3f923ae15b6c7b6e
Dzerjinski Lemos <dzerjinski.lemos@zafir.co.ao>
2024-05-10 09:57:13 +0100

    feat: Add error handling page

--- error.vue
+++ error.vue
@@ -1,11 +1,44 @@
+<script setup lang="ts">
+import type { NuxtError } from "#app";
+
+const props = defineProps({
+	error: Object as () => NuxtError,
+});
+</script>
+
 <template>
-  <button @click="handleError">Clear errors</button>
+	<div class="ffa container">
+		<template v-if="error.statusCode === 404">
+			<div class="ffa container">
+				<h1 class>
+					<span class="erro d-block fs-3">404</span>
+					Page cannot be found
+				</h1>
+				<p>Sorry, we're unable to bring you the page you're looking for. Please try:</p>
+				<ul>
+					<li>Double checking the url</li>
+					<li>Hitting the refresh button in your browser</li>
+				</ul>
+				<p>
+					Alternatively, please visit the F&aacute;tima Freitas e Associados
+					<a href="/">homepage</a>.
+				</p>
+			</div>
+		</template>
+		<template v-else>
+			<h1 class>
+				<span class="erro d-block fs-3">{{ errror.statusCode }}</span>
+				Oops, something went wrong.
+			</h1>
+			<p>Try to refresh this page or feel free to contact us if the problem persists.</p>
+		</template>
+	</div>
 </template>
 
-<script setup>
-const props = defineProps({
-  error: Object
-})
-
-const handleError = () => clearError({ redirect: '/' })
-</script>
+<style scoped>
+.container {
+	margin: 0 auto;
+	max-width: 600px;
+	padding: 5em 1rem;
+}
+</style>
