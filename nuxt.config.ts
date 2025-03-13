3d626a240c204a3b69dd9479ef687a440db59b9e
Dzerjinski Lemos <dzerjinski.lemos@zafir.co.ao>
2025-03-13 16:09:49 +0100

    upgrade nuxt-security

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,104 +2,106 @@ import { defineNuxtConfig } from "nuxt/config";
 import { contentSecurityPolicy } from "./csp";
 
 export default defineNuxtConfig({
- app: {
-					head: {
-									script: [
-													{
-																	src: "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js",
-																	integrity:
-																					"sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p",
-																	crossorigin: "anonymous",
-																	tagPosition: "bodyClose",
-													},
-													{
-																	src: "https://www.googletagmanager.com/gtag/js?id=G-3XMWQYWMNL",
-																	async: true,
-													},
-													{
-																	innerHTML: `
-																					window.dataLayer = window.dataLayer || [];
-																					function gtag(){dataLayer.push(arguments);}
-																					gtag('js', new Date());
-																					gtag('config', 'G-3XMWQYWMNL');
-																	`,
-													},
-									],
-									link: [
-													{
-																	rel: "icon",
-																	type: "image/jpg",
-																	href: "/images/ffa-icon.jpg",
-													},
-													{
-																	rel: "stylesheet",
-																	href: "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css",
-																	integrity:
-																					"sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3",
-																	crossorigin: "anonymous",
-													},
+	app: {
+		head: {
+			script: [
+				{
+					src: "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js",
+					integrity:
+						"sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p",
+					crossorigin: "anonymous",
+					tagPosition: "bodyClose",
+				},
+				{
+					src: "https://www.googletagmanager.com/gtag/js?id=G-3XMWQYWMNL",
+					async: true,
+				},
+				{
+					innerHTML: `
+					window.dataLayer = window.dataLayer || [];
+					function gtag(){
+					   dataLayer.push(arguments);
+					}
+					gtag('js', new Date());
+					gtag('config', 'G-3XMWQYWMNL');
+					`,
+				},
+			],
+			link: [
+				{
+					rel: "icon",
+					type: "image/jpg",
+					href: "/images/ffa-icon.jpg",
+				},
+				{
+					rel: "stylesheet",
+					href: "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css",
+					integrity:
+						"sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3",
+					crossorigin: "anonymous",
+				},
 
-													{
-																	rel: "stylesheet",
-																	href: "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.0/font/bootstrap-icons.css",
-													},
-													{
-																	rel: "stylesheet",
-																	href: "https://fonts.googleapis.com/css?family=Roboto:300,400,500,600,700,800,900&display=swap&subset=latin-ext",
-													},
-													{
-																	rel: "stylesheet",
-																	href: "https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700,800,900&display=swap&subset=latin-ext",
-													},
-									],
-									meta: [
-													{
-																	name: "google-site-verification",
-																	content: "FzmuBkAwgSF4FRuXXVGfZk7SSHesRgRfb6ZLt6HnpgM",
-													},
-									],
-					},
+				{
+					rel: "stylesheet",
+					href: "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.0/font/bootstrap-icons.css",
+				},
+				{
+					rel: "stylesheet",
+					href: "https://fonts.googleapis.com/css?family=Roboto:300,400,500,600,700,800,900&display=swap&subset=latin-ext",
+				},
+				{
+					rel: "stylesheet",
+					href: "https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700,800,900&display=swap&subset=latin-ext",
+				},
+			],
+			meta: [
+				{
+					name: "google-site-verification",
+					content: "FzmuBkAwgSF4FRuXXVGfZk7SSHesRgRfb6ZLt6HnpgM",
+				},
+			],
+		},
 	},
 
- css: ["~~/assets/sass/fatimafreitas.scss", "~~/assets/css/tailwind.css"],
- modules: ["nuxt-security", "@nuxt/devtools", "@nuxtjs/tailwindcss"],
+	css: ["~~/assets/sass/fatimafreitas.scss", "~~/assets/css/tailwind.css"],
+	modules: ["nuxt-security", "@nuxt/devtools", "@nuxtjs/tailwindcss"],
 
- routeRules: {
-					"/a/**": { appMiddleware: ["auth-guard"], ssr: false },
+	routeRules: {
+		"/a/**": { appMiddleware: ["auth-guard"], ssr: false },
 	},
 
- runtimeConfig: {
-					public: {
-									antboxUrl: "",
-									recaptchaSiteKey: "",
-									applicantsKey: "",
-									tinyMceApiKey: "",
-					},
+	runtimeConfig: {
+		public: {
+			antboxUrl: "",
+			recaptchaSiteKey: "",
+			applicantsKey: "",
+			tinyMceApiKey: "",
+		},
 
-					recaptchaSecretKey: "",
+		recaptchaSecretKey: "",
 	},
 
- security: {
-					corsHandler: false,
-					csrf: {
-									enabled: true,
-									methodsToProtect: ["PATCH", "POST", "PUT", "DELETE"],
-					},
-					nonce: true, // Enables HTML nonce support in SSR mode
-					sri: true,
+	security: {
+		corsHandler: false,
+		csrf: {
+			enabled: true,
+			methodsToProtect: ["PATCH", "POST", "PUT", "DELETE"],
+		},
+		nonce: true, // Enables HTML nonce support in SSR mode
+		sri: true,
 
-					headers: {
-									contentSecurityPolicy,
-									crossOriginEmbedderPolicy: "unsafe-none",
-									crossOriginOpenerPolicy: "unsafe-none",
-									crossOriginResourcePolicy: "cross-origin",
-					},
+		headers: {
+			contentSecurityPolicy,
+			crossOriginEmbedderPolicy: "unsafe-none",
+			crossOriginOpenerPolicy: "unsafe-none",
+			crossOriginResourcePolicy: "cross-origin",
+		},
 	},
 
- sourcemap: {
-					server: true,
-					client: true,
+	sourcemap: {
+		server: true,
+		client: true,
 	},
 
- compatibilityDate: "2024-07-16",
+	compatibilityDate: "2024-07-16",
 });
